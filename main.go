ed56305d9c78de54409687617b030133e2caa84f
James Cho <mrjamesjcho@gmail.com>
2025-04-30 19:51:35 -0700

    add server

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	// This is a simple Go program that prints "Hello, World!" to the console.
-	fmt.Println("Hello, World!")
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found in environment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Println("Starting server on port", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+	fmt.Println("PORT:", portString)
 }
