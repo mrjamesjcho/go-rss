b1152d5cbea0bf6f2bc11f0b6c1ae68bb762d2bf
James Cho <mrjamesjcho@gmail.com>
2025-05-05 15:59:05 -0700

    add feed follows handler and endpoint

---
+++ handler_feed_follows.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/mrjamesjcho/go-rss/internal/database"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		FeedID uuid.UUID `json:"feed_id"`
+	}
+
+	params := parameters{}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Invalid request payload: %v", err.Error()))
+		return
+	}
+
+	feedFollow, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+		ID:     uuid.New(),
+		FeedID: params.FeedID,
+		UserID: user.ID,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Failed to create feed follow: %v", err.Error()))
+		return
+	}
+
+	respondWithJson(w, http.StatusCreated, databaseFeedFollowToFeedFollow(feedFollow))
+}
