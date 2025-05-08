ad11472264216ce93049e052d970ce8338726128
James Cho <mrjamesjcho@gmail.com>
2025-05-07 19:28:24 -0700

    fix posts table migration

--- sql/schema/006_posts.sql
+++ sql/schema/006_posts.sql
@@ -5,8 +5,8 @@ CREATE TABLE posts (
     published_at TIMESTAMP NOT NULL,
     title TEXT NOT NULL,
     url TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
     UNIQUE (feed_id, url),
     feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
 );
