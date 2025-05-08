455c7858e049692ae0d7f7ca7177c55585b3e4e5
James Cho <mrjamesjcho@gmail.com>
2025-05-07 19:29:54 -0700

    add user posts handler and endpoint

--- handler_user.go
+++ handler_user.go
@@ -35,3 +35,16 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	respondWithJson(w, http.StatusOK, databaseUserToUser(user))
 }
+
+func (apiCfg *apiConfig) handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User) {
+	posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  10,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Failed to get posts for user: %v", err.Error()))
+		return
+	}
+
+	respondWithJson(w, http.StatusOK, databasePostsToPosts(posts))
+}
